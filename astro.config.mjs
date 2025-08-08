29138d5eca00ab1e5773f077d00171f43f278b7b
Todd Bittner <tbittner@coastalchicago.com>
2025-08-08 15:29:03 -0500

    Added a site property for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://tbittner-cc.github.io',
     vite: {
         plugins: [tailwindcss()],
     },
