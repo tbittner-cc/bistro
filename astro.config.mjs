e5d08675890d0d8d0d11ca48d3da17afd3c31f16
Todd Bittner <tbittner@coastalchicago.com>
2025-08-09 15:00:18 -0500

    Second attempt for relative site links.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tbittner-cc.github.io',
+    base: '/bistro',
     vite: {
         plugins: [tailwindcss()],
     },
